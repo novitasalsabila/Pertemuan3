345ba6d0145358e4ba6d36253d7691c945fdcc4c
novitasalsabila <novnovv10@gmail.com>
2023-03-13 12:05:06 +0700

    Tambah kondisi

---
+++ latihan2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+double rerata(double a,double b){
+    return (a+b)/2;
+}
+string status (double c){
+    if (c >= 60)
+        return "Lulus";
+    else
+        return "gagal";
+}
+string status2 (double rata, double nil){
+    if (rata >= 60 || nil >= 70)
+        return "Lulus";
+    else
+        return "gagal";
+}
+int main(){
+    double nilM,nilB;
+    cout << "Masukan nilai Matematika = ";
+    cin >> nilM;
+    cout << "Masukan nilai Bahasa Indonesia = ";
+    cin >> nilB;
+    cout << "Status kelulusan = " << status (rerata(nilM,nilB));
+    cout << "\nStatus kelulusan = "<< status2 (rerata(nilM,nilB),nilM);
+    return 0;
+}
